45346d264395d453db1770a1a27ea7189efa4c9d
David Young-chan Kay <davidykay@gmail.com>
2021-08-05 17:02:04 -0700

    Initial import

---
+++ src/js/hello.js
@@ -0,0 +1,75 @@
+//import ThreeGlobe from 'three-globe';
+
+//const myGlobe = new ThreeGlobe()
+//    .globeImageUrl(<imageUrl>)
+//    .pointsData(<myData>);
+//
+//const myScene = new THREE.Scene();
+//myScene.add(myGlobe);
+
+
+// Gen random data
+const N = 5;
+const gData = [...Array(N).keys()].map(() => ({
+  lat: (Math.random() - 0.5) * 180,
+  lng: (Math.random() - 0.5) * 360,
+  size: Math.random() / 3,
+  color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+}));
+
+const arcsData = [...Array(N).keys()].map(() => ({
+  startLat: (Math.random() - 0.5) * 180,
+  startLng: (Math.random() - 0.5) * 360,
+  endLat: (Math.random() - 0.5) * 180,
+  endLng: (Math.random() - 0.5) * 360,
+  color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+}));
+
+const pathsData = [
+  [[40, 40, 0],
+   [50, 50, 0]]];
+
+const Globe = new ThreeGlobe()
+.globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
+.bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+//.pointsData(gData)
+.pathsData(pathsData)
+//.arcsData(arcsData)
+.pointAltitude('size')
+.pointColor('color');
+
+//setTimeout(() => {
+//  gData.forEach(d => d.size = Math.random());
+//  Globe.pointsData(gData);
+//}, 4000);
+
+// Setup renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('globeViz').appendChild(renderer.domElement);
+
+// Setup scene
+const scene = new THREE.Scene();
+scene.add(Globe);
+scene.add(new THREE.AmbientLight(0xbbbbbb));
+scene.add(new THREE.DirectionalLight(0xffffff, 0.6));
+
+// Setup camera
+const camera = new THREE.PerspectiveCamera();
+camera.aspect = window.innerWidth/window.innerHeight;
+camera.updateProjectionMatrix();
+camera.position.z = 500;
+
+// Add camera controls
+const tbControls = new THREE.TrackballControls(camera, renderer.domElement);
+tbControls.minDistance = 101;
+tbControls.rotateSpeed = 5;
+tbControls.zoomSpeed = 0.8;
+
+// Kick-off renderer
+(function animate() { // IIFE
+  // Frame cycle
+  tbControls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+})();
